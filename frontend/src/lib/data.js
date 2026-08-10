bb0aaa350fdff3094db77633fe0f664f3dfb57c9
dixita-emp9 <emp9@foreachnext.com>
2026-08-10 13:51:32 +0530

    Fix filename case for heroRiver image

--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -31,7 +31,7 @@ export const IMAGES = {
   // heroRiver:
   //   "https://images.unsplash.com/photo-1755781367841-224a84f4a0bc?crop=entropy&cs=srgb&fm=jpg&ixid=M3w4NjA2MDV8MHwxfHNlYXJjaHwzfHxIaW5kdSUyMEFhcnRpJTIwZGl5YXMlMjByaXZlciUyMHN1bnNldHxlbnwwfHx8fDE3ODU1NjkwMjJ8MA&ixlib=rb-4.1.0&q=85",
   heroRiver:
-    "/about.jpg",
+    "/about.JPG",
   aarti:
     "https://images.unsplash.com/photo-1750759213152-cc80468c21a6?crop=entropy&cs=srgb&fm=jpg&ixid=M3w4NjA2MDV8MHwxfHNlYXJjaHwxfHxIaW5kdSUyMEFhcnRpJTIwZGl5YXMlMjByaXZlciUyMHN1bnNldHxlbnwwfHx8fDE3ODU1NjkwMjJ8MA&ixlib=rb-4.1.0&q=85",
   heritage:
